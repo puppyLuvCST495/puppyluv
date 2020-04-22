7964f7b7ff88647667b52a9029673302f63f71a7
Pernille Dahl <pdahl@csumb.edu>
2020-04-22 15:12:57 -0700

    added profile update

--- PuppyLuv/ProfileViewController.swift
+++ PuppyLuv/ProfileViewController.swift
@@ -5,13 +5,14 @@
 //  Created by Pernille Dahl on 3/16/20.
 //  Copyright © 2020 Athena Enosara. All rights reserved.
 //
-
 import UIKit
-
-class ProfileViewController: UIViewController {
+import AlamofireImage
+import Parse
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    
-  
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var displayNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -19,6 +20,21 @@ class ProfileViewController: UIViewController {
         //makeRound()
         // Do any additional setup after loading the view.
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let user = PFUser()
+        let query = PFQuery(className: "User")
+        query.includeKeys(["author", "profile_img"])
+        let imageFile = user["profile_img"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+//
+        profileImageView.af_setImage(withURL: url)
+        displayNameLabel.text = PFUser.current()?["username"] as? String
+        descriptionLabel.text = PFUser.current()?["profile_description"] as? String
+    }
+    
+    
     
 //    func makeRound(){
 //        profileImage?.layer.cornerRadius = (profileImage?.frame.size.width ?? 0.0) / 2
@@ -26,20 +42,12 @@ class ProfileViewController: UIViewController {
 //        profileImage?.layer.borderWidth = 3.0
 //        profileImage?.layer.borderColor = UIColor.white.cgColor
 //    }
-    
-    
-    
-    
-    
-
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
     */
-
 }
--- PuppyLuv/SettingCameraViewController.swift
+++ PuppyLuv/SettingCameraViewController.swift
@@ -5,75 +5,73 @@
 //  Created by Athena Enosara on 4/13/20.
 //  Copyright © 2020 Athena Enosara. All rights reserved.
 //
-
 import UIKit
 import AlamofireImage
 import Parse
-
 class SettingCameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
     @IBAction func submitImageButton(_ sender: Any) {
-    //        let user = PFObject(className: "User")
-    //
-    //        let imageData = settingImageView.image!.pngData()
-    //        let file = PFFileObject(data: imageData!)
-    //
-    //        user["profile_img"] = file
-    //
-    //        user.saveInBackground{ (success, error) in
-    //            if success{
-                    dismiss(animated: true, completion: nil)
-    //                print("saved!")
-    //            } else {
-    //                print("error!")
-    //            }
-    //        }
+//            let user = PFObject(className: "User")
+//
+        let imageData = settingImageView.image!.pngData()
+//            let file = PFFileObject(name: "profileImage.png", data: imageData!)
+//
+//            user["profile_img"] = file
+//            user["author"] = PFUser.current()
+//
+//            user.saveInBackground{ (success, error) in
+//                if success{
+//                    self.dismiss(animated: true, completion: nil)
+//                    print("saved!")
+//                } else {
+//                    print("error!")
+//                    print(error as Any)
+//                }
+//            }
+        let imageFile = PFFileObject(name: "profileImage.png", data: imageData!)
+//        imageFile?.save()
+        let user = PFUser.current()
+        user?.setObject(imageFile as Any, forKey: "profile_img")
+        user?.saveInBackground(block: { (success, error) in
+            if(success){
+                print("saved!")
+                self.dismiss(animated: true, completion: nil)
+                print("dismissing view")
+            } else {
+                print("error!")
+                print(error as Any)
+            }
+        })
     }
-    
-
     @IBOutlet weak var settingImageView: UIImageView!
+    
     @IBAction func onTapCameraButton(_ sender: Any) {
         let picker = UIImagePickerController()
         picker.delegate = self
         picker.allowsEditing = true
-
         if UIImagePickerController.isSourceTypeAvailable(.camera){
             picker.sourceType = .camera
         } else {
             picker.sourceType = .photoLibrary
         }
-
         present(picker, animated: true, completion: nil)
     }
-
-    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
-        
         let size = CGSize(width: 300, height: 300)
         let scaledImage = image.af_imageScaled(to: size)
-        
         settingImageView.image = scaledImage
-        
         dismiss(animated: true, completion: nil)
-        
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-    
-
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
     */
-
 }

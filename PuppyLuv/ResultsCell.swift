21201d1aa241d5d59ba4eb2ce205b56fea84774b
Pernille Dahl <pdahl@csumb.edu>
2020-04-22 12:56:36 -0700

    trying to fix this conflict

---
+++ PuppyLuv/ResultsCell.swift
@@ -0,0 +1,31 @@
+//
+//  ResultsCell.swift
+//  PuppyLuv
+//
+//  Created by Rosario on 4/21/20.
+//  Copyright © 2020 Athena Enosara. All rights reserved.
+//
+
+import UIKit
+import Parse
+class ResultsCell: UITableViewCell {
+    
+    @IBOutlet weak var ResultImage: UIImageView!
+    
+    
+
+    
+    
+ override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+

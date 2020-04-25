3541654a92b449e14be50a847b316ccf0b78c825
Pernille Dahl <pdahl@csumb.edu>
2020-04-24 19:09:02 -0700

    fixed size of image

--- PuppyLuv/FeedCameraViewController.swift
+++ PuppyLuv/FeedCameraViewController.swift
@@ -55,7 +55,7 @@ class FeedCameraViewController: UIViewController, UIImagePickerControllerDelegat
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
-        let size = CGSize(width: 150, height: 250)
+        let size = CGSize(width: 300, height: 300)
         let scaledImage = image.af_imageAspectScaled(toFill: size)
         
         imageView.image = scaledImage
